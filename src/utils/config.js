4ff2b4743e6a8febed4c8c1f8191b2e4a95505f1
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-07 12:37:57 +0000

    another try for authentication

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,15 +1,18 @@
 const auth_endpoint = "https://accounts.spotify.com/authorize";
 
 const { VITE_CLIENT_ID } = import.meta.env;
-
-const scopes = [
+const { VITE_REDIRECT_URI } = import.meta.env;
+/*const scopes = [
   "user-top-read",
   "user-read-currently-playing",
   "user-read-recently-played",
   "user-read-private",
-];
-
-export const spotify_popup_url =
+];*/
+const scopes = "user-read-private user-top-read user-read-currently-playing user-read-recently-played"; // Adjust scopes as needed
+/*export const spotify_popup_url =
   `${auth_endpoint}?client_id=${VITE_CLIENT_ID}` +
   `&redirect_uri=${window.location.origin}/Spoti-Auth/redirect&response_type=token` +
-  `&scope=${scopes.join("%20")}&show_dialog=true`;
+  `&scope=${scopes.join("%20")}&show_dialog=true`;*/
+
+export const spotify_popup_url = `https://accounts.spotify.com/authorize?client_id=${VITE_CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(VITE_REDIRECT_URI)}&scope=${encodeURIComponent(scopes)}`;
+  //window.location.href = authUrl;
